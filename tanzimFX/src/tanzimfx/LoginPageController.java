734027ca28867e9c4bd0368214afd4dcb32a14e2
Mohammed-Aljablai <mohammed.aljablai@gmail.com>
2024-10-10 21:35:51 +0300

    Create LoginPageController.java

---
+++ tanzimFX/src/tanzimfx/LoginPageController.java
@@ -0,0 +1,40 @@
+/*
+ - This project call tanzim.
+ - tanzim: is a project where orgnize the stuednt and the doctor, in addtion
+   the emplyee life> from sending msg update degree and more...
+ - this project have been worked by:
+   - Mohammed Abdullh Al-jablai
+   - Gamal Aldeen Al-hakl
+   - Youseef Al-asadi
+   - abdAlmalik
+*/
+package tanzimfx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXToggleButton;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+
+public class LoginPageController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
